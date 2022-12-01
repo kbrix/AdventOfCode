584a493cdc7447afa399f3a869d840b33740024b
kbrix <kbrix@protonmail.com>
2022-12-01 20:33:47 +0100

    Solve problem 1, 2022

---
+++ Year2022/Problem1.fs
@@ -0,0 +1,22 @@
+﻿module Year2022.Problem1
+
+let solution (data: string []) (n: int) : int =
+    let sumArray =
+        data
+        |> String.concat ","
+        |> fun stringConcatenated -> stringConcatenated.Split(",,")
+        |> Array.map (fun s ->
+            s.Split(",")
+            |> Array.map System.Convert.ToInt32
+            |> Array.sum)
+        |> Array.sortDescending
+        |> Array.take n
+
+    let sum = Array.sum sumArray
+
+    if n = 1 then
+        printfn $"The elf carrying the most calories is carrying {sum} calories."
+    else
+        printfn $"The {n} elves carrying the most calories are carrying a combined total of {sum} calories."
+
+    sum
---
+++ Year2022Tests/Problem1Tests.fs
@@ -0,0 +1,16 @@
+﻿module Year2022Tests.Problem1Tests
+
+open NUnit.Framework
+open Year2022
+
+[<Test>]
+let Example () =
+    Assert.That(Problem1.solution Problem1Data.exampleData 1, Is.EqualTo(24000))
+
+[<Test>]
+let AnswerPart1 () =
+    Assert.That(Problem1.solution Problem1Data.inputData 1, Is.EqualTo(69289))
+
+[<Test>]
+let AnswerPart2 () =
+    Assert.That(Problem1.solution Problem1Data.inputData 3, Is.EqualTo(205615))
