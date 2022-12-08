349bbd9f3eb8237a3de1811708f736ed99604a53
kbrix <kbrix@protonmail.com>
2022-12-08 18:22:13 +0100

    Solve problem 6, 2022

---
+++ Year2022/Problem6.fs
@@ -0,0 +1,14 @@
+﻿module Year2022.Problem6
+
+let solution (data: string) (marker: int): int =
+    let charArray = data.ToCharArray()
+    let indexOfWindowWithDistinctValues =
+        charArray
+        |> Array.toSeq
+        |> Seq.windowed marker
+        |> Seq.map (fun values ->
+            let arrayLength = values |> Array.length
+            let distinctArrayLength = values |> Array.distinct |> Array.length
+            arrayLength = distinctArrayLength)
+        |> Seq.findIndex (fun x -> x = true)
+    indexOfWindowWithDistinctValues + marker
---
+++ Year2022Tests/Problem6Tests.fs
@@ -0,0 +1,28 @@
+﻿module Year2022Tests.Problem6Tests
+
+open NUnit.Framework
+open Year2022
+
+[<TestCase(7, "mjqjpqmgbljsphdztnvjfqwrcgsmlb")>]
+[<TestCase(5, "bvwbjplbgvbhsrlpgdmjqwftvncz")>]
+[<TestCase(6, "nppdvjthqldpwncqszvftbrmjlhg")>]
+[<TestCase(10, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")>]
+[<TestCase(11, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")>]
+let ExamplePart1 (expectedValue, exmapleData) =
+    Assert.That(Problem6.solution exmapleData 4, Is.EqualTo(expectedValue))
+    
+[<Test>]
+let SolutionPart1 () =
+    Assert.That(Problem6.solution Problem6Data.inputData 4, Is.EqualTo(1042))
+    
+[<TestCase(19, "mjqjpqmgbljsphdztnvjfqwrcgsmlb")>]
+[<TestCase(23, "bvwbjplbgvbhsrlpgdmjqwftvncz")>]
+[<TestCase(23, "nppdvjthqldpwncqszvftbrmjlhg")>]
+[<TestCase(29, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")>]
+[<TestCase(26, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")>]
+let ExamplePart2 (expectedValue, exmapleData) =
+    Assert.That(Problem6.solution exmapleData 14, Is.EqualTo(expectedValue))
+    
+[<Test>]
+let SolutionPart2 () =
+    Assert.That(Problem6.solution Problem6Data.inputData 14, Is.EqualTo(2980))
