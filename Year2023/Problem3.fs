fa9bf68b37495f45771b039efc75bc029d09aa3c
kbrix <kbrix@protonmail.com>
2023-12-07 21:24:57 +0100

    Solve part two of problem 3, 2023

--- Year2023/Problem3.fs
+++ Year2023/Problem3.fs
@@ -16,6 +16,9 @@ let isNumber (x: string) =
         true
     | _ ->
         false
+
+let isStar (x: string) =
+    x = "*"
     
 let adjacentElements i j (data: 'a array2d) =
     let I = (data.GetLength 0) - 1
@@ -119,3 +122,80 @@ let solution (stringData: string array2d) =
         sumPart1 <- sumPart1 + parseAndSumNumbers row
     
     sumPart1
+
+type CoordinateNumberRelation = { Coordinates: (int * int) list; Number: int }
+    
+let solutionVersion2 (stringData: string array2d) =
+    
+    let rec parseCoordinateNumberRelationsRecursive
+        (row: int) (column: int) (relations: CoordinateNumberRelation list) (rowValueList: (bool * int) list):
+        CoordinateNumberRelation list =
+        match rowValueList with
+        | (x, a) :: (y, b) :: (z, c) :: tail when x && y && z ->
+            let number = 100 * a + 10 * b + c
+            let coordinates = [ column .. column + 2 ] |> List.map (fun column -> (row, column))
+            let relation = { Number = number; Coordinates = coordinates }
+            parseCoordinateNumberRelationsRecursive row (column + 3) (relations @ [relation]) tail
+        | (x, a) :: (y, b) :: tail when x && y ->
+            let number = 10 * a + b
+            let coordinates = [ column .. column + 1 ] |> List.map (fun column -> (row, column))
+            let relation = { Number = number; Coordinates = coordinates }
+            parseCoordinateNumberRelationsRecursive row (column + 2) (relations @ [relation]) tail
+        | (x, a) :: tail when x ->
+            let number = a
+            let coordinates = [ (row, column) ]
+            let relation = { Number = number; Coordinates = coordinates }
+            parseCoordinateNumberRelationsRecursive row (column + 1) (relations @ [relation]) tail
+        | (x, _) :: tail when not x ->
+            parseCoordinateNumberRelationsRecursive row (column + 1) relations tail
+        | [] ->
+            relations
+        | _ ->
+            failwith "👻"
+    
+    let I = (stringData.GetLength 0) - 1
+    let J = (stringData.GetLength 1) - 1
+    
+    let coordinateNumberMap =
+        [0 .. I]
+        |> List.map (fun row ->
+            Array.toList stringData[row, *]
+            |> List.map (fun str -> Int32.TryParse str)
+            |> parseCoordinateNumberRelationsRecursive row 0 [])
+        |> List.collect id
+        |> List.map (fun relation ->
+            relation.Coordinates
+            |> List.map (fun coordinate -> coordinate, relation.Number))
+        |> List.collect id
+        |> Map
+    
+    let solution1 =
+        [ for i in 0 .. I do for j in 0 .. J do yield (i, j) ]
+        |> List.filter (fun (i, j) -> isSymbol stringData[i, j])
+        |> List.map (fun (i, j) ->
+            adjacentElements i j stringData
+            |> List.filter (fun (i, j) -> isNumber stringData[i, j])
+            |> fun coordinates ->
+                coordinates
+                |> List.map (fun coordinate -> coordinateNumberMap[coordinate])
+                // Assumes that adjacent numbers are unique (this might not be the case and we might to give them an identifier)
+                |> List.distinct)
+        |> List.collect id
+        |> List.sum
+            
+    let solution2 =
+        [ for i in 0 .. I do for j in 0 .. J do yield (i, j) ]
+        |> List.filter (fun (i, j) -> isStar stringData[i, j]) // Note the different filter function
+        |> List.map (fun (i, j) ->
+            adjacentElements i j stringData
+            |> List.filter (fun (i, j) -> isNumber stringData[i, j])
+            |> fun coordinates ->
+                coordinates
+                |> List.map (fun coordinate -> coordinateNumberMap[coordinate])
+                // Assumes that adjacent numbers are unique (this might not be the case and we might to give them an identifier)
+                |> List.distinct)
+        |> List.map (fun x -> // Instead of collecting the list of numbers, we first compute a product for a given condition 
+            if List.length x = 2 then List.head x * List.last x else 0)
+        |> List.sum
+    
+    solution1, solution2
--- Year2023Tests/Problem3Tests.fs
+++ Year2023Tests/Problem3Tests.fs
@@ -1,15 +1,34 @@
 ﻿module Year2023Tests.Problem3Tests
 
-open System
 open NUnit.Framework
 open Year2023
 
-[<Test>]
+[<Test; Ignore "Use the other version">]
 let ExamplePart1 () =
     let result = Problem3.solution Problem3Data.exampleData
     Assert.That(result, Is.EqualTo(4361))
     
-[<Test>]
+[<Test; Ignore "Use the other version">]
 let SolutionPart1 () =
     let result = Problem3.solution Problem3Data.data
     Assert.That(result, Is.EqualTo(498559))
+
+[<Test>]
+let ExamplePart1Version2 () =
+    let result = fst (Problem3.solutionVersion2 Problem3Data.exampleData)
+    Assert.That(result, Is.EqualTo(4361))
+    
+[<Test>]
+let SolutionPart1Version2 () =
+    let result = fst (Problem3.solutionVersion2 Problem3Data.data)
+    Assert.That(result, Is.EqualTo(498559))
+    
+[<Test>]
+let ExamplePart2Version2 () =
+    let result = snd (Problem3.solutionVersion2 Problem3Data.exampleData)
+    Assert.That(result, Is.EqualTo(467835))
+    
+[<Test>]
+let SolutionPart2Version2 () =
+    let result = snd (Problem3.solutionVersion2 Problem3Data.data)
+    Assert.That(result, Is.EqualTo(72246648))
